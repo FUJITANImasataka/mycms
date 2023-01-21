10113cc3510a93df98b7946c27ce0b6f3cd52ec3
FUJITANImasataka <startallover2008@yahoo.co.jp>
2023-01-21 15:00:40 +0000

    投稿の編集

--- database/migrations/2023_01_18_203339_create_posts_table.php
+++ database/migrations/2023_01_18_203339_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('boby');
+            $table->text('body');
             $table->text('image')->nullable();
             $table->timestamps();
         });
